46cdfa9bd9f8ac3730a6e23e02dca26b7bffb184
Jonas Schätzle <schaetzle.jonas@gmail.com>
2021-03-05 17:55:54 +0100

    Projektdateien hinzufügen.

---
+++ Taschenrechner_Iteration_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner_Iteration_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Erste Zahl: ");
+            int ersteZahl = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Zweite Zahl: ");
+            int zweiteZahl = Convert.ToInt32(Console.ReadLine());
+            int summe = ersteZahl + zweiteZahl;
+            Console.WriteLine($"Resultat: {summe}");
+            Console.ReadLine();
+        }
+    }
+}
